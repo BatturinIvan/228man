c27eb95c731917c488a4bba88a554814dae9df43
BatturinIvan <79141988+BatturinIvan@users.noreply.github.com>
2021-09-27 11:43:04 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-cd /home/box/web/
-sudo gunicorn -c /home/box/web/etc/hello.py hello:app
+
+
